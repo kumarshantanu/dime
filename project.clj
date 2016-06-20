13f0dbf857b5c989f87a2a5bba8da0fabcb4df4f
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-06-21 00:56:53 +0530

    add direct-linking test profile

--- project.clj
+++ project.clj
@@ -14,4 +14,5 @@
              :c18 {:dependencies [[org.clojure/clojure "1.8.0"]]
                    :global-vars {*unchecked-math* :warn-on-boxed}}
              :c19 {:dependencies [[org.clojure/clojure "1.9.0-alpha7"]]
-                   :global-vars {*unchecked-math* :warn-on-boxed}}})
+                   :global-vars {*unchecked-math* :warn-on-boxed}}
+             :dln {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
