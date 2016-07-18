e314d2be46239b8e26d25d9835dd10953167e2ff
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-07-18 14:55:48 +0530

    cleanup old nomenclature - replace var with injectable

--- src/dime/core.clj
+++ src/dime/core.clj
@@ -15,23 +15,24 @@
 
 
 (defmacro partial
-  "Same as clojure.core/partial, except it does not lose associativity with the var."
+  "Same as clojure.core/partial, except it is a macro and is friendly with vars - preserves associativity with vars.
+  Useful to create partials out of vars instrumented using clojure.core/alter-var-root or clojure.core/with-redefs."
   [f & args]
   `(fn [& args#]
      (apply ~f ~@args args#)))
 
 
 (defn process-pre-inject
-  "Return the result of applying pre-inject fn to remaining args if pre-inject is non-nil, pre-injected var
+  "Return the result of applying pre-inject fn to remaining args if pre-inject is non-nil, pre-injected injectable
   otherwise. Default pre-inject processor.
   Arguments:
    pre-inject  ; pre-inject fn
-   the-var     ; the var to be injected with dependencies
+   injectable  ; injectable to be injected with dependencies
    args        ; map to resolve dependencies from"
-  [pre-inject the-var args]
+  [pre-inject injectable args]
   (if pre-inject
-    (pre-inject the-var args)
-    the-var))
+    (pre-inject injectable args)
+    injectable))
 
 
 (defn inject
@@ -49,8 +50,8 @@
   otherwise. Default post-inject processor.
   Arguments:
    post-inject ; post-inject fn
-   injected    ; the partial fn created from the var
-   inject-key  ; inject key for the var
+   injected    ; the partial fn created from the injectable
+   inject-key  ; inject key for the injectable
    resolved    ; resolved map of seed + dependencies so far"
   [post-inject injected inject-key resolved]
   (if post-inject
@@ -59,28 +60,28 @@
 
 
 (defn inject-all
-  "Given a map of name/var pairs and seed data map, resolve/inject all dependencies and return a map of
+  "Given a map of name/injectable pairs and seed data map, resolve/inject all dependencies and return a map of
   name/partially-applied-function pairs."
   ([graph seed {:keys [post-inject-processor]
                 :or {post-inject-processor process-post-inject}
                 :as options}]
     (i/expected map? "a dependency graph as a map" graph)
     (i/expected map? "seed data to begin injection" seed)
-    (reduce (fn inject-one [m [k the-var]]
-              (let [post-inject (fn [m p] (-> (t/post-inject-fn the-var)
+    (reduce (fn inject-one [m [k injectable]]
+              (let [post-inject (fn [m p] (-> (t/post-inject-fn injectable)
                                             (post-inject-processor p k m)))
                     inject-deps (fn [m] (if (contains? m k)         ; avoid duplicate resolution
                                           m
                                           (->> options              ; propagate options for `pre-inject` processing
-                                            (inject the-var m)
+                                            (inject injectable m)
                                             (post-inject m)
                                             (assoc m k))))]
-                (->> (t/dep-keys the-var)                           ; find dependencies of each var
+                (->> (t/dep-keys injectable)                        ; find dependencies of each injectable
                   (map (fn [each-dep-key]                           ; verify each dependency exists in seed/graph
                          (when-not (or (contains? seed each-dep-key)
                                      (contains? graph each-dep-key))
                            (i/expected (format "dependency %s (for %s %s) to exist among seed/graph keys"
-                                         each-dep-key k the-var)
+                                         each-dep-key k injectable)
                              {:seed-keys (keys seed)
                               :graph-keys (keys graph)}))
                          each-dep-key))
